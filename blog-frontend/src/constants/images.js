605c1b28f000370eb7d36e8e60ee258c82291e11
Shyam <shyamnkolge2020@gmail.com>
2024-03-04 10:22:39 +0530

    CTA section added

--- blog-frontend/src/constants/images.js
+++ blog-frontend/src/constants/images.js
@@ -1,8 +1,8 @@
 import Logo from '../assets/logo.png'
 import Hero3 from '../assets/hero-3.png'
 import Hero4 from '../assets/hero-4.png'
-
 import Post1Image from '../assets/posts/post1.jpg'
+import CTAImage from '../assets/CTAImage.jpg'
 
 
 const images = {
@@ -10,6 +10,7 @@ const images = {
     Hero3,
     Hero4,
     Post1Image,
+    CTAImage,
 }
 
 export default images;
--- blog-frontend/src/pages/home/container/Articles.jsx
+++ blog-frontend/src/pages/home/container/Articles.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
 import {ArticleCard} from '../../../components/index.js'
+import { FaArrowRight } from "react-icons/fa6";
+
 
 export const Articles = () => {
   return (
-    <section className='container mx-auto flex flex-wrap md:gap-x-5 gap-y-5 px-5 py-10'>
+    <section className='flex flex-col container mx-auto px-5 py-10'>
+
+      <div className=' flex flex-wrap md:gap-x-5 gap-y-5 pb-10'>
+        <ArticleCard classNames='w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]'/>
+        <ArticleCard classNames='w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]'/>
+        <ArticleCard classNames='w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]'/>     
         <ArticleCard classNames='w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]'/>
         <ArticleCard classNames='w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]'/>
         <ArticleCard classNames='w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]'/>
+      </div>
+
+      <button className='mx-auto flex items-center gap-x-2 font-bold text-primary border-2 border-primary px-6 py-3 rounded-lg'>
+          <span className=''>More Articles </span>
+          <FaArrowRight className='h-3 w-3'/>
+      </button>
+
+      
     </section>
   )
 }
